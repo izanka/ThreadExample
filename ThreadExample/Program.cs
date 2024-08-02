e5eb11a349e36563b9f700e44524a1acd0a30445
izanka <kaliya.iza@gmail.com>
2024-08-02 10:15:06 +0530

    Add project files.

---
+++ ThreadExample/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ThreadExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void MyFunction()
+            {
+                // Some work here
+                Console.WriteLine("Hello, World!");
+            }
+            Thread myThread = new Thread(new ThreadStart(MyFunction));
+            myThread.Start();
+            
+        }
+    }
+}
